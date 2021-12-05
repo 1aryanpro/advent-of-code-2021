3cd41153236e59f709b16b73a09a7516d9d10fe0
SmashMineGame <aryanpro@icloud.com>
2021-12-04 16:05:30 -0800

    completed Day 1

---
+++ day1.py
@@ -0,0 +1,14 @@
+input = open('day1.txt', 'r').read().split('\n')
+arr = [int(y) for y in input[:-1]]
+count1 = count2 = 0
+
+for i in range(1, len(arr)):
+    if arr[i] > arr[i-1]:
+        count1 += 1
+
+for i in range(3, len(arr)):
+    if arr[i] > arr[i-3]:
+        count2 += 1
+
+print(f"{count1 = }")
+print(f"{count2 = }")
