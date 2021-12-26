7666f993e254379c4c2e42f1f246a17045a30fcc
SmashMineGame <aryanpro@icloud.com>
2021-12-26 13:13:30 -0800

    Refactor

--- day12.py
+++ day12.py
@@ -8,37 +8,21 @@ for a,b in input:
     paths[a].append(b)
     paths[b].append(a)
 
-def pathfind(pos = 'start', visited = [], double = False, path = []):
+def pathfind(pos = 'start', visited = [], double = False):
     count = 0
     if pos.islower(): visited.append(pos)
-    path.append(pos)
 
     for b in paths[pos]:
+        cdouble = double
+
         if b == 'end':
             count += 1
-            path.append(b)
-            # print(",".join(path))
             continue
-        if b in visited:
-            if double or b == 'start': continue
-            double = True
-        count += pathfind(b, visited.copy(), double, path.copy())
+
+        if b in visited and cdouble == (cdouble := True) or b == 'start': continue
+
+        count += pathfind(b, visited.copy(), cdouble)
     return count
 
-def pathfind2(pos = 'start', visited = [], double = False, path = []):
-    if pos == 'end':
-        path.append(pos)
-        # print(','.join(path))
-        return 1
-    if pos in visited:
-        if double or pos == 'start': return 0
-        double = True
-
-    if pos.islower():
-        visited.append(pos)
-    path.append(pos)
-
-    return sum(pathfind2(next, visited.copy(), double, path.copy()) for next in paths[pos])
-
 # print(paths)
-print(pathfind2())
+print(pathfind())
